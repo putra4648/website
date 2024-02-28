08947cb938faa2a0c67003740c5401b8f83b11d3
putra4648 <wakwaujaim@gmail.com>
2024-02-28 11:47:20 +0700

    fix configuration

--- src/main/java/com/putra/portfolio/config/AppConfig.java
+++ src/main/java/com/putra/portfolio/config/AppConfig.java
@@ -3,24 +3,30 @@ package com.putra.portfolio.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class AppConfiguration {
+public class AppConfig {
 
     private final String[] ACCEPTED_URLS = new String[] {
             "/",
             "/portfolio",
             "/work",
+            "/download-cv",
             "/webjars/**"
     };
+
+    @Bean
+    AuthenticationEntryPoint authenticationEntryPoint() {
+        return new NoPopupEntryPointConfig();
+    }
 
     @Bean
     ModelMapper mapper() {
@@ -43,15 +49,16 @@ public class AppConfiguration {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .headers(headers -> {
+                    headers.frameOptions(options -> options.sameOrigin());
+                })
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(
                         auth -> auth.requestMatchers(ACCEPTED_URLS).permitAll()
                                 .anyRequest().authenticated())
                 .userDetailsService(userDetailsService())
-                .formLogin(login -> {
-                    login.loginPage("/login");
-                })
-                .httpBasic(Customizer.withDefaults());
+                .formLogin(login -> login.disable())
+                .httpBasic(basic -> basic.authenticationEntryPoint(authenticationEntryPoint()));
         return http.build();
     }
 
