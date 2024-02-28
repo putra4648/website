94a9f718d83124461efaeccdedaa734988589114
putra4648 <wakwaujaim@gmail.com>
2024-02-28 11:48:53 +0700

    add download CV API

--- src/main/java/com/putra/portfolio/controller/PageController.java
+++ src/main/java/com/putra/portfolio/controller/PageController.java
@@ -1,7 +1,15 @@
 package com.putra.portfolio.controller;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -23,6 +31,29 @@ public class PageController {
     public ModelAndView workPage(ModelMap model) {
         model.put("isActive", true);
         return new ModelAndView("portfolio.html", model);
+    }
+
+    @GetMapping("/contact")
+    public ModelAndView contactPage(ModelMap model) {
+        model.put("isActive", true);
+        return new ModelAndView("contact.html", model);
+    }
+
+    @GetMapping("/download-cv")
+    public ResponseEntity<?> downloadCV() {
+        try {
+            File file = new File("C:\\Users\\muham\\Downloads\\test.pdf");
+            byte[] res = FileCopyUtils.copyToByteArray(file);
+            return ResponseEntity
+                    .ok()
+
+                    .contentType(MediaType.APPLICATION_PDF)
+                    .body(new ByteArrayResource(res));
+        } catch (Exception e) {
+            Map<String, Object> body = new HashMap<>();
+            body.put("message", e.getLocalizedMessage());
+            return ResponseEntity.internalServerError().body(body);
+        }
     }
 
 }
