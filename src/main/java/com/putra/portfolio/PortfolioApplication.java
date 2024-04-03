48cbd616e0f06531821058c46cd8594da3292897
putra4648 <wakwaujaim@gmail.com>
2024-04-03 19:22:51 +0700

    refactor: change frontend to use module based

--- src/main/java/com/putra/portfolio/PortfolioApplication.java
+++ src/main/java/com/putra/portfolio/PortfolioApplication.java
@@ -8,8 +8,8 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
 @EnableMongoAuditing
 public class PortfolioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PortfolioApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(PortfolioApplication.class, args);
+  }
 
 }
