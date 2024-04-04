86fcd55deb89bada8f8e23c27a30eb0bf7933749
putra4648 <wakwaujaim@gmail.com>
2024-04-04 12:20:47 +0700

    refactor: change route download-cv

--- src/main/java/com/putra/portfolio/config/AppConfig.java
+++ src/main/java/com/putra/portfolio/config/AppConfig.java
@@ -14,40 +14,40 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class AppConfig {
-    private final String[] ACCEPTED_URLS_NON_USER = new String[] {
-            "/api/v1/register",
-            "/api/v1/forgot-password",
-    };
+  private final String[] ACCEPTED_URLS_NON_USER = new String[] {
+      "/api/v1/register",
+      "/api/v1/forgot-password",
+      "/api/v1/download-cv"
+  };
 
-    private final String[] ACCEPTED_URLS_ANY_ROLE = new String[] {
-            "/api/v1/portfolio",
-            "/api/v1/contacts",
-            "/api/v1/experience",
-            "/api/v1/download-cv"
-    };
+  private final String[] ACCEPTED_URLS_ANY_ROLE = new String[] {
+      "/api/v1/portfolio",
+      "/api/v1/contacts",
+      "/api/v1/experience",
+  };
 
-    @Bean
-    ModelMapper mapper() {
-        return new ModelMapper();
-    }
+  @Bean
+  ModelMapper mapper() {
+    return new ModelMapper();
+  }
 
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(
-                        auth -> auth
-                                .requestMatchers(ACCEPTED_URLS_NON_USER).permitAll()
-                                .requestMatchers(HttpMethod.GET, ACCEPTED_URLS_ANY_ROLE).hasRole("USER")
-                                .requestMatchers(ACCEPTED_URLS_ANY_ROLE).hasRole("SUPERADMIN")
-                                .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
-        return http.build();
-    }
+  @Bean
+  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(
+            auth -> auth
+                .requestMatchers(ACCEPTED_URLS_NON_USER).permitAll()
+                .requestMatchers(HttpMethod.GET, ACCEPTED_URLS_ANY_ROLE).hasRole("USER")
+                .requestMatchers(ACCEPTED_URLS_ANY_ROLE).hasRole("SUPERADMIN")
+                .anyRequest().authenticated())
+        .formLogin(Customizer.withDefaults())
+        .httpBasic(Customizer.withDefaults());
+    return http.build();
+  }
 }
