7e5f37da865ebd61289070c5958622a0928e24c2
putra4648 <wakwaujaim@gmail.com>
2025-02-18 18:53:31 +0700

    chore:: test deploy

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,112 +1,110 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import type * as Preset from "@docusaurus/preset-classic";
+import type { Config } from "@docusaurus/types";
+import { themes as prismThemes } from "prism-react-renderer";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-    title: 'Putra Coderz',
-    tagline: 'Hello there 👋',
+  title: "Putra Coderz",
+  tagline: "Hello there 👋",
 
-    favicon: 'img/favicon.ico',
+  favicon: "img/favicon.ico",
 
-    // Set the production url of your site here
-    url: 'https://personal-website-production-d8cc.up.railway.app/',
-    // Set the /<baseUrl>/ pathname under which your site is served
-    // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/',
+  // Set the production url of your site here
+  url: "https://personal-website-production-d8cc.up.railway.app/",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
 
-    // GitHub pages deployment config.
-    // If you aren't using GitHub pages, you don't need these.
-    //organizationName: 'putra4648', // Usually your GitHub org/user name.
-    //projectName: 'personal-website', // Usually your repo name.
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "putra4648", // Usually your GitHub org/user name.
+  projectName: "website", // Usually your repo name.
 
-    onBrokenLinks: 'throw',
-    onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
-    // Even if you don't use internationalization, you can use this field to set
-    // useful metadata like html lang. For example, if your site is Chinese, you
-    // may want to replace "en" with "zh-Hans".
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en'],
-    },
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
 
-    presets: [
-        [
-            'classic',
-            {
-                docs: {
-                    sidebarPath: './sidebars.ts',
-                    editUrl: 'https://github.com/putra4648/personal-website/website',
-                    showLastUpdateAuthor: false,
-                    showLastUpdateTime: false
-                },
-                blog: {
-                    showReadingTime: true,
-                    feedOptions: {
-                        type: ['rss', 'atom'],
-                        xslt: true,
-                    },
-                    editUrl: 'https://github.com/putra4648/personal-website',
-                    // Useful options to enforce blogging best practices
-                    onInlineTags: 'warn',
-                    onInlineAuthors: 'warn',
-                    onUntruncatedBlogPosts: 'warn',
-                },
-                theme: {
-                    customCss: './src/css/custom.css',
-                },
-            } satisfies Preset.Options,
-        ],
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          editUrl: "https://github.com/putra4648/website/tree/master",
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ["rss", "atom"],
+            xslt: true,
+          },
+          editUrl: "https://github.com/putra4648/website/tree/master",
+          // Useful options to enforce blogging best practices
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
     ],
+  ],
 
-    themeConfig: {
-        // Replace with your project's social card
-        image: 'img/docusaurus-social-card.jpg',
-        navbar: {
-            title: 'Putra Coderz',
-            logo: {
-                alt: 'My Site Logo',
-                src: 'img/favicon.ico',
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      title: "Putra Coderz",
+      logo: {
+        alt: "My Site Logo",
+        src: "img/favicon.ico",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Tutorial",
+        },
+        { to: "/blog", label: "Blog", position: "left" },
+      ],
+    },
+    footer: {
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Tutorial",
+              to: "/docs/intro",
             },
-            items: [
-                {
-                    type: 'docSidebar',
-                    sidebarId: 'tutorialSidebar',
-                    position: 'left',
-                    label: 'Tutorial',
-                },
-                {to: '/blog', label: 'Blog', position: 'left'},
-            ],
+            {
+              label: "Projects",
+              to: "/docs/projects",
+            },
+            {
+              label: "Tips & Tricks",
+              to: "/docs/tips&tricks",
+            },
+          ],
         },
-        footer: {
-            links: [
-                {
-                    title: 'Docs',
-                    items: [
-                        {
-                            label: 'Tutorial',
-                            to: '/docs/intro',
-                        },
-                        {
-                            label: 'Projects',
-                            to: '/docs/projects',
-                        },
-                        {
-                            label: 'Tips & Tricks',
-                            to: '/docs/tips&tricks',
-                        }
-                    ],
-                },
-            ],
-            copyright: `Copyright © ${new Date().getFullYear()} Putra Coderz. Built with Docusaurus.`,
-        },
-        prism: {
-            theme: prismThemes.github,
-            darkTheme: prismThemes.dracula,
-        },
-    } satisfies Preset.ThemeConfig,
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Putra Coderz. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
 };
 
 export default config;
