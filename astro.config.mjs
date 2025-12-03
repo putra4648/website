af2dde7e311308c85ebd177e3a4e9cf4ec65b733
Putra <wakwaujaim@gmail.com>
2025-12-03 07:16:37 +0000

    refactor: minor

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,17 @@ import { defineConfig } from 'astro/config';
 
 import vercel from '@astrojs/vercel';
 
+import vue from '@astrojs/vue';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), vue()],
   adapter: vercel(),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
