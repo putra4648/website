b80230b34314c04c54dfb973846f08bfa062eebc
putra4648 <wakwaujaim@gmail.com>
2026-04-14 10:42:24 +0700

    feat: add global site constants for title and description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "PutraCoderz";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "Experienced Fullstack Developer focused on digitalizing manufacturing workflows. Proven success in increasing operational efficiency by 50% using modern Java and JavaScript stacks";
