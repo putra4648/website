e0417ba6f5036b207227e98ddf2236e0f7e1e309
Putra <wakwaujaim@gmail.com>
2026-09-11 03:46:53 +0000

    feat: update work exp, themes

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,5 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
+import { z } from 'astro/zod'
 import { glob } from "astro/loaders";
 
 const blog = defineCollection({
@@ -8,7 +9,7 @@ const blog = defineCollection({
   schema: ({ image }) =>
     z.object({
       title: z.string(),
-      description: z.array(z.string()),
+      description: z.string(),
       pubDate: z.coerce.date(),
       updatedDate: z.coerce.date().optional(),
       heroImage: image().optional(),
