03fdcc9f06b1ccb036c8bfc167138bed784a1413
Putra <wakwaujaim@gmail.com>
2026-09-11 08:06:30 +0000

    feat: add background image for Open Graph and clean up unused imports

--- src/pages/open-graph/[...route].ts
+++ src/pages/open-graph/[...route].ts
@@ -1,20 +1,25 @@
-// src/pages/open-graph/[...route].ts
-
 import { OGImageRoute } from 'astro-og-canvas';
-
 import { getCollection } from 'astro:content';
 
-const collectionEntries = await getCollection('blog');
-
-// Map the array of content collection entries to create an object.
-// Converts [{ id: 'post.md', data: { title: 'Example', description: '' } }]
-// to { 'post.md': { title: 'Example', description: '' } }
-const pages = Object.fromEntries(collectionEntries.map(({ id, data }) => [id, data]));
+const blogs = await getCollection('blog');
+const pages = Object.fromEntries(
+    blogs.map((post) => [
+        post.id, // Sesuaikan dengan key yang dipanggil di tag <img> (post.id)
+        {
+            title: post.data.title,
+            description: post.data.description || '',
+        },
+    ])
+);
 
 export const { getStaticPaths, GET } = await OGImageRoute({
-    pages: pages,
+    pages,
     getImageOptions: (path, page) => ({
         title: page.title,
         description: page.description,
+        bgImage: {
+            path: './src/assets/bg-blog.jpg',
+            fit: "fill"
+        },
     }),
 });
