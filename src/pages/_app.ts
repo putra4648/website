f8799313a31c232875bc860bd87ae93079c9cd69
Putra <wakwaujaim@gmail.com>
2026-09-11 03:56:47 +0000

    chore: update dependencies and remove unused particles integration in _app.ts

--- src/pages/_app.ts
+++ src/pages/_app.ts
@@ -1,15 +1,6 @@
 import type { App } from 'vue';
-import Particles from "@tsparticles/vue3";
-import { loadSlim } from "@tsparticles/slim"
 import { MotionPlugin } from 'motion-v';
 
 export default (app: App) => {
-    if (typeof window !== 'undefined') {
-        app.use(Particles, {
-            init: async (engine) => {
-                await loadSlim(engine);
-            },
-        });
-    }
     app.use(MotionPlugin)
 };
